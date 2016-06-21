eb329e06f783a59c5f6eee0dd34c4ef0f9160e76
Amit Kumar <y13uc022@lnmiit.ac.in>
2016-06-21 20:02:30 +0530

    Added new class MessageAppApplication and set it as launcher class in Androidmenifest.xml

---
+++ app/src/main/java/com/example/amit/onetimemessageviewer/MessegeAppApplication.java
@@ -0,0 +1,10 @@
+package com.example.amit.onetimemessageviewer;
+
+import android.app.Application;
+
+/**
+ * Created by amit on 21/6/16.
+ */
+public class MessegeAppApplication extends Application {
+
+}
